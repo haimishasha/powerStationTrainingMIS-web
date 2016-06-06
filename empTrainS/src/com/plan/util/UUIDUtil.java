e9ef75de80341b55d66d7cb914fd194515d8f4b7
ding <ding@ding-PC>
2016-06-06 23:54:07 +0800

    修改uuid

--- empTrainS/src/com/plan/util/UUIDUtil.java
+++ empTrainS/src/com/plan/util/UUIDUtil.java
@@ -2,7 +2,7 @@ package com.plan.util;
 
 import java.util.UUID;
 
-public class uuid {
+public class UUIDUtil {
 	public static String getUUid(){
 		UUID uuid  =  UUID.randomUUID(); 
 		String s = UUID.randomUUID().toString();
