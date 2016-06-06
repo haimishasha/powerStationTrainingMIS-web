3b2362a4ec623247df2255c4564edacdbc967710
Forward_cto <18435155316@163.com>
2016-06-06 23:57:29 +0800

    恢复uuid

--- empTrainS/src/com/plan/util/uuid.java
+++ empTrainS/src/com/plan/util/uuid.java
@@ -2,7 +2,7 @@ package com.plan.util;
 
 import java.util.UUID;
 
-public class UUIDUtil {
+public class uuid {
 	public static String getUUid(){
 		UUID uuid  =  UUID.randomUUID(); 
 		String s = UUID.randomUUID().toString();
